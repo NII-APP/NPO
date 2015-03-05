490ae620ee92e2f5322efaa4f483289e66fa6ed4
vlzh92 <vlzh92@gmail.com>
2015-03-05 11:38:59 +0300

    Add matrix function

--- NPO/engine/CGL/ccolumnchart.cpp
+++ NPO/engine/CGL/ccolumnchart.cpp
@@ -29,12 +29,13 @@ void CColumnChart::setData(const CMatrix& d) {
 }
 
 void CColumnChart::paintCGL() {
+    qDebug() << "paint MAC" << data.width() << data.height();
     qreal x0(0.5 * (data.width() - size));
     qreal y0(0.5 * (data.height() - size));
     for (int i(0); i != data.width(); ++i) {
         for (int j(0); j != data.height(); ++j) {
             QRgb c(color(data[i][j]) | 0xFF000000);
-            glColor4ubv(static_cast<unsigned char*>(static_cast<void*>(&c)));
+            glColor3ubv(static_cast<unsigned char*>(static_cast<void*>(&c)) + 1);
             drowParallelepiped(CParallelepiped(x0 + i + REGILAR_MARGIN, x0 + i + 1 - REGILAR_MARGIN,
                                                scale(data[i][j]), scale(0.0),
                                y0 + j + REGILAR_MARGIN, y0 + j + 1 - REGILAR_MARGIN));
