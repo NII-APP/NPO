f7af5d9d854f9370429fed6079efd3d3d6856906
BOPOHOB <vornon@ya.ru>
2015-09-21 16:44:41 +0300

    move glContect configuration to application cunstructor

--- Program/NPO/main.cpp
+++ Program/NPO/main.cpp
@@ -1,24 +1,11 @@
 #include "application.h"
 #include "mainwindow.h"
-#include "cmatrix.h"
 #include <QDebug>
-#include "c2dchart.h"
-#include "cdimensionarray.h"
-#include "cdimensioninterval.h"
-#include <QDir>
-#include <femwidget.h>
-#include "femviewer.h"
-
 
 int main(int argc, char *argv[])
 {qDebug() << "2rfeWsds78gde";
     Application a(argc, argv);
     setlocale(LC_ALL,"RUS");
-
-    QGLFormat fmt(QGLFormat::defaultFormat());
-    fmt.setSampleBuffers(true);
-    fmt.setSamples(2);
-    QGLFormat::setDefaultFormat(fmt);
 
     MainWindow window;
     window.showMaximized();
